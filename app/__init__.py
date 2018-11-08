c77946539073d6a719ff76c0688868debcdd7221
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2018-11-08 17:10:10 +0100

    Setup basic flask app, config, and manage.py

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from flask import Flask
+from logging.handlers import RotatingFileHandler
+
+from config import Config
+
+
+def create_app(app_settings=Config):
+    app = Flask(__name__)
+    app.config.from_object(app_settings)
+
+    # Initialize Extensions
+
+    # Register Blueprints
+
+    # Logging
+    if not app.debug and not app.config['TESTING']:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/app.log',
+                                           maxBytes=app.config['MAX_LOG_SIZE'],
+                                           backupCount=app.config['MAX_LOGS'])
+
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s '
+            '[in %(pathname)s:%(lineno)d'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('App Startup')
+
+    return app
